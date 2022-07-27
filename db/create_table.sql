6a5664c7ff0d3a5badac3d583a38e6b74342b042
hmaria <mh6417a@gmail.com>
2022-07-27 10:18:17 -0400

    created etl code

---
+++ db/create_table.sql
@@ -0,0 +1,40 @@
+-- create tables --
+create table reviews (
+	id serial primary key,
+	product_id int,
+	rating int,
+	date timestamp not null,
+	summary varchar(2000),
+	body varchar(2000),
+	recommend boolean,
+	reported boolean default false,
+	reviewer_name varchar(100),
+	reviewer_email varchar(100),
+	response varchar(2000),
+	helpfulness int
+);
+
+create table photos (
+	id serial primary key,
+	review_id int,
+	url varchar(2000),
+	FOREIGN KEY (review_id)
+		REFERENCES reviews (id)
+);
+
+create table characteristic (
+	id serial primary key,
+	product_id int,
+	name varchar(100)
+);
+
+create table characteristic_review (
+	id serial primary key,
+	characteristic_id int,
+	review_id int,
+	value int,
+	FOREIGN KEY (characteristic_id)
+		REFERENCES characteristic (id),
+	FOREIGN KEY (review_id)
+		REFERENCES reviews (id)
+);
